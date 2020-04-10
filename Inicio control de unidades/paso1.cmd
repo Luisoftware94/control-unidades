66b1843b33f6b6440cb27ecdb659af74035e3c76
Luis Avila Gurrola <luisag5435@gmail.com>
2020-04-10 11:09:40 -0600

    anadiendo bat para inicio

---
+++ Inicio control de unidades/paso1.cmd
@@ -0,0 +1,6 @@
+@echo off
+echo Inicio de Control de unidades
+CD C:\Users\wicho\Desktop\control-unidades\backend
+npm run dev
+pause
+exit
---
+++ Inicio control de unidades/paso2.cmd
@@ -0,0 +1,6 @@
+@echo off
+echo Inicio de Control de unidades
+CD C:\Users\wicho\Desktop\control-unidades\backend\frontend
+npm start
+pause
+exit
